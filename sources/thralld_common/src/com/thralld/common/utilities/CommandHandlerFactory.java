9007129833cc228c44ec97bbfd146b502eb212dd
machiry <makrvcs@gmail.com>
2014-08-01 08:47:13 -0700

    Server thread comments added.

--- sources/thralld_common/src/com/thralld/common/utilities/CommandHandlerFactory.java
+++ sources/thralld_common/src/com/thralld/common/utilities/CommandHandlerFactory.java
@@ -3,17 +3,26 @@
  */
 package com.thralld.common.utilities;
 
+import com.thralld.common.annotations.CanReturnNull;
 import com.thralld.common.aobjects.CommandRequestInfo;
 import com.thralld.common.aobjects.ServerCommandHandler;
 import com.thralld.common.commandhandlers.QueryCommandServerHandler;
 import com.thralld.common.commands.QueryCommandRequestInfo;
 
 /**
+ * This class returns command handlers for all the supported commands
+ * 
  * @author m4kh1ry
  *
  */
 public class CommandHandlerFactory 
 {
+	/***
+	 * This method returns server side command handler for provided RequestInfo
+	 * @param toProcessRequest The target requestInfo to process
+	 * @return ServerCommandHandler capable of handling the command or null if no handler exists.
+	 */
+	@CanReturnNull
 	public static ServerCommandHandler getServerCommandHandler(CommandRequestInfo toProcessRequest)
 	{
 		ServerCommandHandler toRet = null;
--- sources/thralld_server/src/com/thralld/server/core/ServerThread.java
+++ sources/thralld_server/src/com/thralld/server/core/ServerThread.java
@@ -2,6 +2,7 @@ package com.thralld.server.core;
 
 import java.util.ArrayList;
 
+import com.thralld.common.annotations.CanReturnNull;
 import com.thralld.common.aobjects.CommandRequestInfo;
 import com.thralld.common.aobjects.CommandResponseInfo;
 import com.thralld.common.aobjects.NetworkConnection;
@@ -9,18 +10,40 @@ import com.thralld.common.aobjects.ServerCommandHandler;
 import com.thralld.common.logging.Logger;
 import com.thralld.common.utilities.CommandHandlerFactory;
 
+/***
+ * This class represents ServerThread:
+ * A thread which will be created for each connected client.
+ * 
+ * This thread works like message processing system.
+ * A request that needs to be processed will be inserted into toProcessCommands, from which the thread reads, processes it
+ * and inserts corresponding responses to commandResults.
+ * 
+ * @author m4kh1ry
+ *
+ */
 public class ServerThread extends Thread 
 {
+	//List containing the commands that need to be processed.
 	private ArrayList<CommandRequestInfo> toProcessCommands = new ArrayList<CommandRequestInfo>();
+	//List containing responses for each of the command
 	private ArrayList<CommandResponseInfo> commandResults = new ArrayList<CommandResponseInfo>();
+	//The Network connection on which the communication needs to be take place.
 	private NetworkConnection targetNetworkConnecion = null;
+	//This indicates if an error occurred while processing a request.
 	public boolean isErrorOccured = true;
+	//Message queue poll time interval.
+	private static final int pollTimeMilliseconds = 1000;
 	
 	public ServerThread(NetworkConnection netConn)
 	{
 		this.targetNetworkConnecion = netConn;
 	}
 	
+	/***
+	 * This method is used to insert requestInfo objects which needs to be processed by thread.
+	 * @param comm RequestInfo object that need to be inserted.
+	 * @return True, if the object is not already present, False, if the object is already present in the queue.
+	 */
 	public boolean addCommand(CommandRequestInfo comm)
 	{
 		synchronized (toProcessCommands) 
@@ -34,6 +57,12 @@ public class ServerThread extends Thread
 		return false;
 	}
 	
+	/***
+	 * This method returns response object(if present) for a given unique id 
+	 * @param uniqueID unique id of the response.
+	 * @return ResponseInfo if present or null
+	 */
+	@CanReturnNull
 	public CommandResponseInfo getResponse(String uniqueID)
 	{
 		CommandResponseInfo toRet = null;
@@ -55,15 +84,27 @@ public class ServerThread extends Thread
 		return toRet;
 	}
 	
+	/***
+	 *The main message request processing method.
+	 */
 	public void run()
 	{
 		while(true)
 		{
 			CommandRequestInfo toProcess = null;
+			//1. Wait till you see any command.
 			while(toProcessCommands.size() == 0)
 			{
-				
+				//Do nothing. Just sleep.
+				try 
+				{
+					Thread.sleep(pollTimeMilliseconds);
+				} catch (InterruptedException e) 
+				{
+					Logger.logException("Error occured while sleeping", e);
+				}
 			}
+			//2. One you see a command, try to get the command.
 			synchronized(toProcessCommands)
 			{
 				if(toProcessCommands.size() > 0)
@@ -71,6 +112,7 @@ public class ServerThread extends Thread
 					toProcess = toProcessCommands.remove(0);
 				}
 			}
+			//3. If you have a command, process it.
 			if(toProcess != null)
 			{
 				ServerCommandHandler commandHandler = CommandHandlerFactory.getServerCommandHandler(toProcess);
@@ -79,6 +121,8 @@ public class ServerThread extends Thread
 				{
 					Logger.logError("Problem occured while processing command:"+toProcess.toString() +" on network connection:"+targetNetworkConnecion.toString());
 					isErrorOccured = true;
+					//End of processing. An Error occurred here.
+					//We will stop and the thread will be killed.
 					break;
 				}
 				synchronized (commandResults) 
